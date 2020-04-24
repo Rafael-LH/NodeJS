3d14a768a30b8b74c598c3678756191c671abfa7
Rafael Lopez <rafael.iskoat@gmail.com>
2020-04-24 13:23:00 -0500

    Respuestas coherentes

--- server.js
+++ server.js
@@ -5,30 +5,41 @@ const bodyParser = require('body-parser') //Esta dependencia nos sirve para pode
 const PORT = 3000;
 const app = express();
 
+const response = require('./network/response');
+
 app.use(bodyParser.json()); //Le indicamos que lo queremos usar en forma de json
 
 app.get('/', (req, res) => {
   res.send('Home')
 })
 // de esta manera nuestra aplicación de express respondera a la url / y nos retornara un 'Hola' 
-app.get('/user', function (request, response) { //los dos parametros de una petición request y el response 
+app.get('/user', function (req, res) { //los dos parametros de una petición request y el response 
 
   // http://localhost:3000/user?orderBy=id
 
-  const { status, message } = request.body
-  const query = request.query
-  const header = request.headers
+  const { status, message } = req.body
+  const query = req.query
+  const header = req.headers
 
   console.log(`Status: ${status}\nInfo ${message}`);
   console.log(query);
 
   //Tambien podemos mandar header en la respuesta
-  response.header({
+  res.header({
     "Custo-header": "Nuestro valor personalizado"
   });
   console.log(header);
-  response.send(header); //enviamos el mensaje
+  // response.send(header); //enviamos el mensaje
+  response.success(req, res, 'Todo ha salido bien');
 
+})
+
+app.post('/message', (req, res) => {
+  if (req.query.error == 'ok') {
+    response.error(req, res, 'Ha ocurrido algun error ❌', 400)
+  } else {
+    response.success(req, res, 'Mensaje creado satisfactoriamente!', 201)
+  }
 })
 
 // Para que nuestra aplicación este escuchando las rutas lo hacemos con lo siguiente
