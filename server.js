b61815de9beeda319dc5662805d1e3d737c62f34
Rafael Lopez <rafael.iskoat@gmail.com>
2020-04-24 18:31:43 -0500

    Servir archivos estáticos

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express') //Esta es la forma nativa que tiene node de traerse modulos
 // import express from 'express' //de esta manera tambien podremos traer nuestra dependencia es la manera en que se hace con ecma6
 const bodyParser = require('body-parser') //Esta dependencia nos sirve para poder acceder a la información del request que nos esta llegando
-
+const path = require('path');
 const PORT = 3000;
 const app = express();
 
@@ -9,8 +9,13 @@ const response = require('./network/response');
 
 app.use(bodyParser.json()); //Le indicamos que lo queremos usar en forma de json
 
+const files = path.join(__dirname, './public');
+app.use(express.static(files)); // OR
+// Name project    Name static files html / css / images 
+// app.use('/NodeJS', express.static('./public')) 
+
 app.get('/', (req, res) => {
-  res.send('Home')
+  res.sendFile('index.html', { root: 'public' })
 })
 // de esta manera nuestra aplicación de express respondera a la url / y nos retornara un 'Hola' 
 app.get('/user', function (req, res) { //los dos parametros de una petición request y el response 
